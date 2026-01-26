a3eddd579c4ece3d386eb60e9ba23899d970f727
Milan Gajic <boromirsrb@yahoo.com>
2026-01-26 21:33:33 +0100

    rest

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deabdcd2aea436d62e08a5262aba6db84a24a873")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7734aa4047d28193c386862ddb5bc3911df27010")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
