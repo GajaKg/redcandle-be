3cde15fa7548ee79f154e65c4c36a6a720fc9776
Milan Gajic <boromirsrb@yahoo.com>
2026-06-11 21:07:46 +0200

    fixes

--- API/Dtos/Customer/CustomerUpdateDto.cs
+++ API/Dtos/Customer/CustomerUpdateDto.cs
@@ -5,6 +5,6 @@ namespace API.Dtos.Customer
         public string Name { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public string Contact { get; set; } = string.Empty;
-        public string Note { get; set; } = string.Empty;
+        public string? Note { get; set; } = string.Empty;
     }
 }
--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc2d372007e657cd6caf4b7f8631635c2369ac3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8ab586b36703263edd69a6973a16f80ff041522")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
