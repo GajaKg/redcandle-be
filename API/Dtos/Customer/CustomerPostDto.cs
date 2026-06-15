d8a2aa5a3ebcb73f564a2109d0f9d2c3071d4cb4
Milan Gajic <boromirsrb@yahoo.com>
2026-06-15 19:10:57 +0200

    edist customer post dto

--- API/Dtos/Customer/CustomerPostDto.cs
+++ API/Dtos/Customer/CustomerPostDto.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos.Customer
 {
     public class CustomerPostDto
     {
+        [Required]
+        [MinLength(3)]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
         public string Address { get; set; } = string.Empty;
+
+        [Required]
         public string Contact { get; set; } = string.Empty;
+
         public string? Note { get; set; } = string.Empty;
     }
 }
